b7722fa026681d2a708c72941f74806229f03609
boytalaba <ivanchristianespiritu123@gmail.com>
2022-05-03 18:32:07 +0800

    mint1

--- backend/src/config.js
+++ backend/src/config.js
@@ -57,8 +57,8 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0xab69ae2B3E62779Ac6711f792a92BF4806BF74fb';
 const TREASURY_ADDRESS = '0xab69ae2B3E62779Ac6711f792a92BF4806BF74fb';
 const MAX_SUPPLY = 3333; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 5; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const MINT_PRICE = 0.1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-03-09T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
